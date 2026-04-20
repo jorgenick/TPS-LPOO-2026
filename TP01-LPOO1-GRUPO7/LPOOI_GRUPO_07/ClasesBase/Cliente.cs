987a57aa924069639357b88427e88adf084b5243
unknown <65787321+jorgenick@users.noreply.github.com>
2026-04-20 17:58:53 -0300

    creacion de clases Rol y usuario

---
+++ TP01-LPOO1-GRUPO7/LPOOI_GRUPO_07/ClasesBase/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    class Cliente
+    {
+        public string Cli_DNI { get; set; }
+        public string Cli_Apellido { get; set; }
+        public string Cli_Nombre { get; set; }
+        public string Cli_Direccion { get; set; }
+        public string OS_CUIT { get; set; }
+        public string Cli_NroCarnet { get; set; }
+
+    }
+
+
+
+}
